40d6cf2f4b6026dce2cf449c75bec36f9382a361
erica-l-s <erica.l.s1996@gmail.com>
2024-03-08 19:04:03 -0300

    database building

--- ecommerce-admin/app/layout.tsx
+++ ecommerce-admin/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ModalProvider } from "@/providers/modal-provider";
 import "./globals.css";
+import prismadb from "@/lib/prismadb";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const store = prismadb.store.update
   return (
     <ClerkProvider>
     <html lang="en">
