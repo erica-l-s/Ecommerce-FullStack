3a6609597916de4533d34f2ddb5b42a6353567ac
erica-l-s <erica.l.s1996@gmail.com>
2024-03-24 18:13:59 -0300

    forms settings

--- ecommerce-admin/components/main-nav.tsx
+++ ecommerce-admin/components/main-nav.tsx
@@ -1,13 +1,37 @@
 "use client"
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import { useParams, usePathname } from 'next/navigation'
 import React from 'react'
 
 function MainNav({
     className,
     ...props
 }:React.AllHTMLAttributes<HTMLElement>) {
-   
+  const pathname = usePathname()
+  const params = useParams()
+   const routes = [{
+  href: `/${params.storeId}/settings`,
+  label: 'Settings',
+  active: pathname === `/${params.storeId}/settings`
+
+   }]
   return (
-    <div>Main Navigation</div>
+    <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
+     
+     {routes.map((route)=> (
+      <Link key={route.href}
+      href={route.href}
+      className={cn("text-sm font-medium transition-colors hover:text-primary",
+      route.active ? "text-black dark:text-white" : "text-muted-foreground "
+      )}
+     
+      >
+      {route.label}
+      </Link>
+     ))}
+     
+    </nav>
   )
 }
 
--- ecommerce-admin/components/navbar.tsx
+++ ecommerce-admin/components/navbar.tsx
@@ -1,15 +1,27 @@
-import { UserButton } from '@clerk/nextjs'
+import { UserButton, auth } from '@clerk/nextjs'
 import React from 'react'
 import MainNav from './main-nav'
+import StoreSwitcher from './store-switcher'
+import { redirect } from 'next/navigation'
+import prismadb from '@/lib/prismadb'
 
-function navbar() {
-  return (
+async function Navbar() {
+  const {userId} = auth()
+
+  if(!userId){
+    redirect("/sign-in")
+
+ }  
+ const stores = await prismadb.store.findMany({
+  where:{
+    userId,
+  }
+})
+ return (
     <div className='border-b'>
         <div className='flex h-16 items-center px-4'>
-        <div>
-            This will be a store switcher
-        </div>
-        <MainNav/>
+       <StoreSwitcher items={stores}/>
+        <MainNav className='mx-6'/>
         <div className='ml-auto flex items-center space-x-4'>
           <UserButton afterSignOutUrl='/'/>
         </div>
@@ -18,4 +30,4 @@ function navbar() {
   )
 }
 
-export default navbar
+export default Navbar
