4fe6373388800a79c34dbcc9ea9658678dfd23de
erica-l-s <erica.l.s1996@gmail.com>
2024-03-29 20:43:33 -0300

    image upload

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -1,10 +1,10 @@
 "use client"
 
 import { AlertModal } from "@/components/modals/alert-modal";
-import { ApiAlert } from "@/components/ui/api-alert";
 import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import Heading from "@/components/ui/heading";
+import ImageUpload from "@/components/ui/image-upload";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { useOrigin } from "@/hooks/use-origin";
@@ -29,49 +29,49 @@ const formSchema = z.object({
 
 type BillboardFormValues = z.infer<typeof formSchema>
 
-interface BillboardFormProps{
-  initialData: Billboard | null  
+interface BillboardFormProps {
+    initialData: Billboard | null
 }
 export const BillboardForm: React.FC<BillboardFormProps> = ({
-initialData
+    initialData
 }) => {
 
     const params = useParams()
     const router = useRouter()
     const origin = useOrigin()
 
-    const [open,setOpen] = useState(false)
-    const [ loading, setLoading] = useState(false)
+    const [open, setOpen] = useState(false)
+    const [loading, setLoading] = useState(false)
 
     const title = initialData ? "Edit billboard" : "Create billboard"
     const description = initialData ? "Edit a billboard" : "Add a new billboard"
     const toastMessage = initialData ? "Billboard updated" : "Billboard created"
     const action = initialData ? "Save changes" : "Create"
-    
+
 
     const form = useForm<BillboardFormValues>({
-        resolver:zodResolver(formSchema),
+        resolver: zodResolver(formSchema),
         defaultValues: initialData || {
-            label:'',
-            imageUrl:''
+            label: '',
+            imageUrl: ''
         }
     })
 
-    const onSubmit = async (data:BillboardFormValues) => {
-        
+    const onSubmit = async (data: BillboardFormValues) => {
+
         try {
             setLoading(true)
-            await axios.patch(`/api/stores/${params.storeId}`,data)
+            await axios.patch(`/api/stores/${params.storeId}`, data)
             router.refresh()
             toast.success("Store updated.")
         } catch (error) {
-           toast.error("Something went wrong.") 
-        }finally{
+            toast.error("Something went wrong.")
+        } finally {
             setLoading(false)
         }
     }
 
-    const onDelete = async () =>{
+    const onDelete = async () => {
         try {
             setLoading(true)
             await axios.delete(`/api/stores/${params.storeId}`)
@@ -80,65 +80,90 @@ initialData
             toast.success("Store deleted")
         } catch (error) {
             toast.error("Make sure you removed all products and categories first?")
-        }finally{
+        } finally {
             setLoading(false)
             setOpen(false)
         }
     }
-    return(
+    return (
         <>
-        <AlertModal 
-        isOpen={open}
-        onClose={()=> setOpen(false)}
-        onConfirm={onDelete}
-        loading={loading}
-        />
-        <div className="flex items-center justify-between">
-           <Heading
-           title={title}
-           description={description}
-           />
-           {initialData && (<Button
-           disabled={loading}
-           variant='destructive'
-           size='sm'
-           onClick={()=>{setOpen(true)}}
-           >
-          <Trash className="h-4 w-4"/>
-           </Button>)}
-           
-        </div>
-        <Separator/>
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
-             <div className="grid grid-cols-3 gap-8">
-              <FormField
-              control={form.control}
-              name="label" 
-              render={({field})=>(
-                <FormItem>
-                    <FormLabel>Label</FormLabel>
-                    <FormControl>
-                        <Input disabled={loading} placeholder="Billboard label" {...field}/>
-                    </FormControl>
-                    <FormMessage/>
-                </FormItem>
-              )}
-              />
+            <AlertModal
+                isOpen={open}
+                onClose={() => setOpen(false)}
+                onConfirm={onDelete}
+                loading={loading}
+            />
+            <div className="flex items-center justify-between">
+                <Heading
+                    title={title}
+                    description={description}
+                />
+                {initialData && (<Button
+                    disabled={loading}
+                    variant='destructive'
+                    size='sm'
+                    onClick={() => { setOpen(true) }}
+                >
+                    <Trash className="h-4 w-4" />
+                </Button>)}
 
-              
-             </div>
-             <Button disabled={loading} className="ml-auto" type="submit" >
-                {action}
-             </Button>
-            </form>
+            </div>
+            <Separator />
 
-        </Form>
-        <Separator/>
-       
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+                    <div className="grid grid-cols-3 gap-8">
+                        <FormField
+                            control={form.control}
+                            name="imageUrl"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Background image</FormLabel>
+                                    <FormControl>
+                                        <ImageUpload
+                                            value={field.value ? [field.value] : []}
+                                             disable={loading}
+                                             onChange={(url)=>field.onChange(url)}
+                                             onRemove={()=>field.onChange("")}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                       />
+                    </div>
+                    </form>
+            </Form>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+                    <div className="grid grid-cols-3 gap-8">
+                        <FormField
+                            control={form.control}
+                            name="label"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Label</FormLabel>
+                                    <FormControl>
+                                        <Input disabled={loading} placeholder="Billboard label" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+
+
+                    </div>
+                    <Button disabled={loading} className="ml-auto" type="submit" >
+                        {action}
+                    </Button>
+                </form>
+
+            </Form>
+            <Separator />
+
         </>
     )
 }
- 
+
 
 
--- ecommerce-admin/components/ui/image-upload.tsx
+++ ecommerce-admin/components/ui/image-upload.tsx
@@ -1,16 +1,19 @@
 "use client"
 
+import { Image, ImagePlus, Trash } from "lucide-react"
 import { useEffect, useState } from "react"
+import { Button } from "./button"
+import { CldUploadWidget } from "next-cloudinary"
 
 interface ImageUploadProps {
     disable?: boolean
-    onChange:(value:string) => void
-    onRemove:(value:string) => void
-    value:string[]
+    onChange: (value: string) => void
+    onRemove: (value: string) => void
+    value: string[]
 }
 
 
-const ImagUpload:React.FC<ImageUploadProps> = ({
+const ImageUpload: React.FC<ImageUploadProps> = ({
     disable,
     onChange,
     onRemove,
@@ -18,26 +21,61 @@ const ImagUpload:React.FC<ImageUploadProps> = ({
 }) => {
     const [isMounted, setMounted] = useState(false)
 
-    useEffect(()=>{
+    useEffect(() => {
         setMounted(true)
-    },[])
-  
+    }, [])
 
-    const onUpload = (result:any) =>{
+
+    const onUpload = (result: any) => {
         onChange(result.info.secure_url)
     }
 
-    if(!isMounted){
+    if (!isMounted) {
         return null
     }
 
-  return (
-    <div>
-     <div className="mb-4 flex items-center gap-4">
+    return (
+        <div>
+            <div className="mb-4 flex items-center gap-4">
+                {value.map((url) => {
+                    <div key={url} className="relative w-[200px] h-[200px] rounded-md overflow-hidden">
+                        <div className="z-10 zbsolute top-2 right-2">
+                            <Button type="button" onClick={() => onRemove(url)} variant="destructive" size="icon">
+                                <Trash className="h-4 w-4" />
+                            </Button>
+                        </div>
+                        <Image
+                            fill
+                            className="object-cover"
+                            alt="Image"
+                            src={url}
+                        />
+                    </div>
 
-     </div>
-    </div>
-  )
+                })}
+            </div>
+
+            <CldUploadWidget onUpload={onUpload} uploadPreset="xrdvapib">
+                {({ open }) => {
+                    const onClick = () => {
+                        open()
+                    }
+                    return (
+                        <Button
+                            type="button"
+                            disabled={disable}
+                            variant="secondary"
+                            onClick={onClick}
+                        >
+                            <ImagePlus className="h-4 w-4 mr-2" />
+                            Upload an image
+                        </Button>
+                    )
+                }}
+
+            </CldUploadWidget>
+        </div>
+    )
 }
 
-export default ImagUpload
+export default ImageUpload
