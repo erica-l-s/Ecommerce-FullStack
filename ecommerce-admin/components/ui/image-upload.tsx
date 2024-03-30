33210e75bfd4c098286fc2c9d7f40db4248b5eb9
erica-l-s <erica.l.s1996@gmail.com>
2024-03-29 21:38:46 -0300

    upload image

--- ecommerce-admin/components/ui/image-upload.tsx
+++ ecommerce-admin/components/ui/image-upload.tsx
@@ -1,9 +1,10 @@
 "use client"
 
-import { Image, ImagePlus, Trash } from "lucide-react"
+import { ImagePlus, Trash } from "lucide-react"
 import { useEffect, useState } from "react"
 import { Button } from "./button"
 import { CldUploadWidget } from "next-cloudinary"
+import Image from "next/image"
 
 interface ImageUploadProps {
     disable?: boolean
@@ -37,10 +38,14 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
     return (
         <div>
             <div className="mb-4 flex items-center gap-4">
-                {value.map((url) => {
+                {value.map((url) => (
                     <div key={url} className="relative w-[200px] h-[200px] rounded-md overflow-hidden">
                         <div className="z-10 zbsolute top-2 right-2">
-                            <Button type="button" onClick={() => onRemove(url)} variant="destructive" size="icon">
+                            <Button 
+                            type="button" 
+                            onClick={() => onRemove(url)} 
+                            variant="destructive" 
+                            size="icon">
                                 <Trash className="h-4 w-4" />
                             </Button>
                         </div>
@@ -52,7 +57,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
                         />
                     </div>
 
-                })}
+                ))}
             </div>
 
             <CldUploadWidget onUpload={onUpload} uploadPreset="xrdvapib">
