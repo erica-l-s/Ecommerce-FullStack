15f3a2ead83bf0a815fb932582b3406e86b436ee
erica-l-s <erica.l.s1996@gmail.com>
2024-03-31 16:17:46 -0300

    making forms category

---
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import Heading from "@/components/ui/heading"
+import { Plus } from "lucide-react"
+import { useParams, useRouter } from "next/navigation"
+import { CategoryColumn, columns } from "./columns"
+import { Separator } from "@/components/ui/separator"
+import { DataTable } from "@/components/ui/data-table"
+import { ApiList } from "@/components/ui/api-list"
+
+interface CategoryClientProps {
+    data: CategoryColumn[]
+}
+
+export const CategoryClient: React.FC<CategoryClientProps> = ({
+    data
+}) => {
+    const router = useRouter()
+    const params = useParams()
+
+    return (
+        <>
+            <div className="flex items-center justify-between">
+                <Heading
+                    title={`Categories (${data.length})`}
+                    description="Manage categories for your store" />
+                <Button onClick={() => router.push(`/${params.storeId}/categories/new`)}>
+                    <Plus className="mr-2 h-4 w-4" />
+                    Add New
+                </Button>
+            </div>
+            <Separator />
+            <DataTable searchKey="label" columns={columns} data={data} />
+            <Heading title="API" description="API calls do Categories" />
+            <Separator />
+            <ApiList entityName="categories" entityIdName="categoryId" />
+        </>
+    )
+}
